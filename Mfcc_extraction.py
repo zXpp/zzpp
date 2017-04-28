adf305eaa242d2532b36213df7ccb6d1d7735409
ZXPP <784723438@qq.com>
2017-04-28 08:58:50 +0800

    Update Mfcc_extraction.py

--- Mfcc_extraction.py
+++ Mfcc_extraction.py
@@ -10,7 +10,7 @@ import os
 #import pprint
 import sklearn as sl
 import numpy as np
-import librosa
+import librosa #是专门用来处理音频和音乐的工具箱，功能蛮多的
 import librosa.feature as f
 import librosa.util as lu
  
