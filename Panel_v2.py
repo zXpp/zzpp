b704188d2624be6b6f053c5377304b07337ad9c1
ZXPP <784723438@qq.com>
2017-08-10 21:26:01 +0800

    Add files via upload

--- Panel_v2.py
+++ Panel_v2.py
@@ -6,14 +6,14 @@ Created on Tue Jul 25 15:23:13 2017
 #from __future__ import unicode_literals
 #show=True
 #from __future__ import print_function
-import sys,os,codecs,shutil,tempfile#ctypes,
+import sys,os,codecs,shutil,tempfile,ctypes
 import numpy as np
 from PIL import Image,ImageFilter
 from formater_OK import ppt_diago
 #pypath=sys.argv[2]#os.path.realpath(sys.argv[0])
 #pyrealpath=pypath.encode('utf-8')
 ##print pypath
-#sys.path+=[os.getcwd(),os.path.join(pyrealpath,r"util\pyLib")]
+#sys.path+=[os.getcwd(),os.path.join(pyrealpath,r"guidata\pyLib")]
 #pptabspath=sys.argv[1][2:-1]
 #width=sys.argv[3]
 #raw=sys.argv[4]
@@ -21,7 +21,9 @@ from formater_OK import ppt_diago
 #raw=os.path.splitext(pptabspath)[0]#D:\zx\特性Glance格式转换工具需求分析
 
 #print tmp
-
+def Mbox(title, text, style):
+	return ctypes.windll.user32.MessageBoxW(0, text, title, style)
+#ctypes,
 def renamePngs(path):#tmp/tmp2
 	#dock=[]
 	for root,dirs,files in os.walk(path):	#遍历统计
@@ -56,9 +58,9 @@ def png2pngs(indir,outname):#indir :abso path;outname:abo
 	final_img.save(outname,"PNG")
 
 def pngs2xml(reldirlist,outname,newtit):#reldirlist=relativelist.相对路径的列表
-	with codecs.open(os.path.join(pyrealpath,r"util\Tmp\VAN_Feature_Glance.xml"),'rb') as f1:
+	with codecs.open(os.path.join(pyrealpath,r"guidata\Tmp\VAN_Feature_Glance.xml"),'rb') as f1:
 		content,begflag,endflag,new=f1.read(),'<image href="','</image>',[]
-		titbeg,titend="<title>","</title>"
+		titbeg,titend='<title>','</title>'
 		#content=content.repalce("title>##NAME##</title","title>"+newtit+"</title")
 		#matchline=[line for line in content if begflag in line][0]
 		matchtag=content[content.find(begflag):content.rfind(endflag)+len(endflag)]
@@ -68,13 +70,13 @@ def pngs2xml(reldirlist,outname,newtit):#reldirlist=relativelist.相对路径的
 		titrep=content[:content.find(titbeg)+len(titbeg)]+newtit+content[content.rfind(titend):content.index(begflag)]
 		xmlout=titrep+''.join(new)+content[content.rfind(endflag)+len(endflag):]
 		out1=open(outname,'w')
-		out1.write(xmlout)
+		out1.write(xmlout.encode('gbk'))
 		##print xmlout >out1,outname
 		out1.close()
 def pngs2html(reldirlist,outname,newtit):
-	with codecs.open(os.path.join(pyrealpath,r"util\Tmp\Htmltemp.html"),'rb') as f2:
+	with codecs.open(os.path.join('',r"guidata\Tmp\Htmltemp.html"),'rb') as f2:
 		content,begflag,endflag,new=f2.read(),'<img src="','</br>',[]
-		titbeg,titend="<title>","</title>"
+		titbeg,titend='<title>','</title>'
 		#matchline=[line for line in content if begflag in line][0]
 		#content=content.repalce("title>##NAME##</title","title>"+newtit+"</title")
 		matchtag=content[content.find(begflag):content.rfind(endflag)+len(endflag)]
@@ -84,9 +86,14 @@ def pngs2html(reldirlist,outname,newtit):
 		titrep=content[:content.find(titbeg)+len(titbeg)]+newtit+content[content.rfind(titend):content.index(begflag)]
 		writeout=titrep+"".join(new)+content[content.rfind(endflag)+len(endflag):]
 		out=open(outname,'w')
-		out.write(writeout)
+		out.write(writeout.encode('gbk'))
 		out.close()
 if __name__ == "__main__":
+
+	if getattr(sys,'frozen',False):
+		pyrealpath = sys._MEIPASS
+	else:
+		pyrealpath = os.path.split(os.path.realpath(__file__))[0]
 	import guidata,subprocess
 	_app = guidata.qapplication()
 	if ppt_diago.edit():pass#如果面板被编辑过。
@@ -95,6 +102,7 @@ if __name__ == "__main__":
 	args=ppt_diago.__dict__
 	pptname = args["_PPTnames"][0]
 	label=os.path.basename(pptname).split('.')[0]
+	#print label--文件的名字，后缀之前的部分
 	if pptname.find(" ") < 0 :#if filebam have jongg
 		vbpptname=pptname
 	else:
@@ -105,14 +113,19 @@ if __name__ == "__main__":
 	else:
 		vbout=chr(34)+raw+chr(34)
 	width = args["_newsize"]
-	pyrealpath=os.path.split(sys.argv[0])[0]
+	#print raw---选择的输出路径，在该路径下生成以label 为名称的文件夹
+	#pyrealpath=os.path.split(os.path.realpath(__file__))[0]#'os.path.split(sys.argv[0])[0]
 	pdfout,tmp= 1,os.path.join(raw,label,str(width))
+	#print pyrealpath
 	tmp2 =os.path.join(raw,label,"Slides")
-	newtitle=args["_outname"]
+	newtitle=args["_outname"] if args["_outname"]!= '' else label
+	#print tmp2---slide 的文件夹
 	if 2 not in args["_outFormat"]:pdfout=0
 	##################CHQ RU JIANQIE VB CHENGXU
-	cmd=" ".join([r"C:\Windows\system32\wscript.exe "+os.path.join(pyrealpath,r"util\Tmp\PPT_Panel.vbs"),vbpptname,str(width),vbout,str(pdfout)])
-	child=subprocess.Popen(cmd)
+	cmd=" ".join([os.path.join(pyrealpath,r"guidata\Tmp\wscript.exe ")+os.path.join(pyrealpath,r"guidata\Tmp\PPT_Panel.vbs"),vbpptname,str(width),vbout,str(pdfout)]).encode('gbk')
+	#print cmd
+ #cmd=" ".join([r"C:\Windows\system32\wscript.exe "+os.path.join(pyrealpath,r"guidata\Tmp\PPT_Panel.vbs"),vbpptname,str(width),vbout,str(pdfout)])
+	child=subprocess.Popen(cmd)#
 	child.wait()
 	#print ('sucess')
 	if os.path.exists(tmp) and os.path.exists(tmp2):#710cunzai
@@ -125,17 +138,26 @@ if __name__ == "__main__":
 		pdfname,lpngname,htmlname,xmlname,rlpngname,rawhtmlname,rawxmlname=[os.path.join(outdir,label+name) for name in mergename_Sets]
 		if os.path.isfile("".join([raw,"\\",label,"\\",label,r".pdf"])):
 			shutil.move("".join([raw,"\\",label,"\\",label,r".pdf"]),outdir)
-		pngs2xml(relativepath,xmlname,newtitle)
-		if 0 in args["_outFormat"]:
-			pngs2xml(relativerawpath,rawxmlname,newtitle)
+		try:
+			Mbox(u"我还在运行...",u'不要着急..我还在努力的拼...完成的时候还会看见我..',0)
+			pngs2xml(relativepath,xmlname,newtitle)
+			if 0 in args["_outFormat"]and args["_raw"]:
+				pngs2xml(relativerawpath,rawxmlname,newtitle)
 
-		pngs2html(relativepath,htmlname,newtitle)
-		if 4 in args["_outFormat"] and args["_raw"]:
-			pngs2html(relativerawpath,rawhtmlname,newtitle)
-		if 1 in args["_outFormat"] and args["_resize"]:
-			png2pngs(tmp,lpngname)#Imgarr,lpng=
-		if 1 in args["_outFormat"] and args["_raw"]:
-			png2pngs(tmp2,rlpngname)
+			pngs2html(relativepath,htmlname,newtitle)
+			if 4 in args["_outFormat"] and args["_raw"]:
+				pngs2html(relativerawpath,rawhtmlname,newtitle)
+			if 1 in args["_outFormat"] and args["_resize"]:
+				png2pngs(tmp,lpngname)#Imgarr,lpng=
+			if 1 in args["_outFormat"] and args["_raw"]:
+				png2pngs(tmp2,rlpngname)
+			Mbox(u'哈哈我又出现啦', u'看见我应该就是成功了~~快去输出目录看一下 \n'+raw, 0)
+			#print ('ok')
+		except:
+			print sys.exc_info()
+			Mbox(u'Sorry', u'Something Error,Change filename to English and retry', 1)
+			#raw_input('press anykey to exit')
+
 
 """
 if os.path.isfile(pdfname) and os.path.isfile(lpngname) and os.path.isfile(htmlname) and os.path.isfile(xmlname):
